27a0b3de4d7bf9b76d1db733f2b50bdd2d820a93
Ömer Faruk Baysal <150450002+omerfarukbaysal04@users.noreply.github.com>
2024-08-01 20:01:26 +0300

    GameData created.

---
+++ GameData.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class GameData
+{
+    public float dolarPuan;
+    public float euroPuan;
+    public float altinPuan;
+    public float tlPuan;
+    public float mutluluk;
+    public int hafta;
+    public int ay;
+    public int yil;
+    public float eskiDolar;
+    public float eskiEuro;
+    public float eskiAltin;
+    public float euroDegisim;
+    public float altinDegisim;
+    public float dolarDegisim;
+    public float vergiOran;
+    public float vergiGeliri;
+     public int güvenlikLevel;
+    public int güvenlikMaliyet;
+    public int saglikLevel;
+    public int saglikMaliyet;
+    public int egitimLevel;
+    public int egitimMaliyet;
+    public int dinLevel;
+    public int dinMaliyet;
+    public int tarimLevel;
+    public int tarimMaliyet;
+    public int teknolojiLevel;
+    public int teknolojiMaliyet;
+    public int genelLevel;
+    public int genelMaliyet;
+    public int genelGelistirmeSayisi;
+    public int gelistirmeAylikGider;
+    public int egitimGelir;
+    public int tarimGelir;
+    public int teknolojiGelir;
+    public int güvenlikGider;
+    public int saglikGider;
+    public int dinGider;
+    public int egitimGider;
+    public int tarimGider;
+    public int teknolojiGider;
+    public int gerekliGelistirme;
+    public int digerGelirler;
+    public int giderler;
+
+    
+}
+
